8d1d412cbb695a61c5f0e4dc3da57fb13bc2bb8b
hiralinda <lilirezerata@gmail.com>
2025-05-11 11:03:44 -0300

    update description experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -16,7 +16,7 @@ const experiences = [
     company: "55PBX Cloud PBX",
     period: "Oct 2024 - Present",
     description:
-      "Currently working as part of a team to maintain and refactor a Cloud PBX system using Node.js, React, Angular, MySQL, and MongoDB. Collaborates in an Agile environment with Scrum practices, using Jira for task management and Bitbucket for version control. Tests APIs with Postman, manages CI/CD pipelines, writes technical documentation, troubleshoots and resolves bugs, and develops new features to enhance system functionality and user experience.",
+      "Currently working as part of a team to maintain and refactor a Cloud PBX system using Node.js, React, Angular, MySQL, and MongoDB. Collaborating in an Agile environment with Scrum practices, using Jira for task management and Bitbucket for version control. Testing APIs with Postman, managing CI/CD pipelines, writing technical documentation, troubleshooting and resolving bugs, and developing new features to enhance system functionality and user experience.",
     color: "#61DAFB",
   },
   {
