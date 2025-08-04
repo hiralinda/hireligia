aa7ec78247910ce94f72378253a3b7037ab012dc
hiralinda <ligiahirata@proton.me>
2025-08-04 19:50:09 -0300

    duolingo

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,48 +1,60 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
-const faqs = [
-  {
-    question: "What are your hobbies and interests outside of work?",
-    answer:
-      "I like reading books and challenge myself to read more books each year than the last. Photography is another hobby I enjoy, particularly taking portraits. I also value spending quality time with friends and family and make it a point to travel whenever possible.",
-  },
-  {
-    question: "What’s your favorite book?",
-    answer:
-      "It is hard to pick a favorite. I would say the Harry Potter series for fictional and The Wild Truth by Carine McCandless for non-fictional.",
-  },
-  {
-    question: "What’s the most interesting place you’ve traveled to?",
-    answer:
-      "My favorite destination so far has been the Amazon Rainforest in Brazil. I visited in 2023 and gained a deep appreciation for the world's largest forest, as well as a richer understanding of my own brazilian culture.",
-  },
-  {
-    question: "Do you have any unique skills or talents?",
-    answer:
-      "I can solve the Rubik's Cube and have even taught kids how to do it. I believe that with dedication and practice, anyone can master it.",
-  },
-  {
-    question:
-      "Did you really make the Rubik's Cube that is displayed (larger screens only) on the Hero section?",
-    answer: (
-      <>
-        Yes, I did using Blender Software and following{" "}
-        <a
-          href="https://www.youtube.com/watch?v=KIuH7zxHfPQ"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500 hover:underline">
-          this tutorial
-        </a>{" "}
-        on YouTube.
-      </>
-    ),
-  },
-];
-
 const FAQ = () => {
   const [expandedIndex, setExpandedIndex] = useState(null);
+
+  const streakStartDate = new Date("2024-01-18T00:00:00"); 
+  const currentDate = new Date();
+
+  const timeDifference = currentDate.getTime() - streakStartDate.getTime();
+
+  const duolingoStreak = Math.ceil(timeDifference / (1000 * 3600 * 24));
+
+  const faqs = [
+    {
+      question: "What are your hobbies and interests outside of work?",
+      answer:
+        "I like reading books and challenge myself to read more books each year than the last. Photography is another hobby I enjoy, particularly taking portraits. I also value spending quality time with friends and family and make it a point to travel whenever possible.",
+    },
+    {
+      question: "What's your Duolingo streak?",
+      answer: `As of today, my Duolingo streak is ${duolingoStreak} days. I'm committed to learning German and found a fun way to keep up with it!.`,
+    },
+    {
+      question: "What’s your favorite book?",
+      answer:
+        "It is hard to pick a favorite. I would say the Harry Potter series for fictional and The Wild Truth by Carine McCandless for non-fictional.",
+    },
+    {
+      question: "What’s the most interesting place you’ve traveled to?",
+      answer:
+        "My favorite destination so far has been the Amazon Rainforest in Brazil. I visited in 2023 and gained a deep appreciation for the world's largest forest, as well as a richer understanding of my own brazilian culture.",
+    },
+    {
+      question: "Do you have any unique skills or talents?",
+      answer:
+        "I can solve the Rubik's Cube and have even taught kids how to do it. I believe that with dedication and practice, anyone can master it.",
+    },
+    {
+      question:
+        "Did you really make the Rubik's Cube that is displayed (larger screens only) on the Hero section?",
+      answer: (
+        <>
+          Yes, I did using Blender Software and following{" "}
+          <a
+            href="https://www.youtube.com/watch?v=KIuH7zxHfPQ"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline"
+          >
+            this tutorial
+          </a>{" "}
+          on YouTube.
+        </>
+      ),
+    },
+  ];
 
   const handleToggle = (index) => {
     setExpandedIndex(expandedIndex === index ? null : index);
@@ -61,16 +73,19 @@ const FAQ = () => {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: index * 0.1 }}
-              className="bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-lg p-6 rounded-xl shadow-lg border border-gray-700">
+              className="bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-lg p-6 rounded-xl shadow-lg border border-gray-700"
+            >
               <button
                 className="w-full text-left focus:outline-none group"
-                onClick={() => handleToggle(index)}>
+                onClick={() => handleToggle(index)}
+              >
                 <h4 className="text-xl font-semibold flex justify-between items-center text-gray-100 group-hover:text-purple-400 transition-colors duration-300">
                   {faq.question}
                   <motion.span
                     animate={{ rotate: expandedIndex === index ? 45 : 0 }}
                     transition={{ duration: 0.3 }}
-                    className="text-purple-400 text-3xl">
+                    className="text-purple-400 text-3xl"
+                  >
                     +
                   </motion.span>
                 </h4>
@@ -81,7 +96,8 @@ const FAQ = () => {
                     initial={{ opacity: 0, height: 0 }}
                     animate={{ opacity: 1, height: "auto" }}
                     exit={{ opacity: 0, height: 0 }}
-                    transition={{ duration: 0.3 }}>
+                    transition={{ duration: 0.3 }}
+                  >
                     <p className="text-lg mt-4 text-gray-300 leading-relaxed">
                       {faq.answer}
                     </p>
