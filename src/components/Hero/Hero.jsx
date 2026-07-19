4f81c0e9681e6a99fd173b11b38f2c5ba3069ced
hiralinda <ligiahirata@proton.me>
2026-07-19 12:00:30 -0300

    remove cv file button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -81,7 +81,7 @@ const Hero = () => {
                 <FaEnvelope />
               </motion.a>
             </motion.div>
-            <motion.div
+            {/* <motion.div
               initial={{ y: -20, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{ delay: 0.8, duration: 0.5 }}
@@ -95,7 +95,7 @@ const Hero = () => {
                 <FaFileDownload className="mr-2" />
                 View CV
               </motion.a>
-            </motion.div>
+            </motion.div> */}
           </div>
           <motion.div
             initial={{ x: 100, opacity: 0 }}
